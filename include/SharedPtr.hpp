4de19de3d901632001e9a4f33ae3541d2a4c6248
Kotherine <astapchukep@student.bmstu.ru>
2021-12-11 19:17:45 +0300

    final version

--- include/SharedPtr.hpp
+++ include/SharedPtr.hpp
@@ -3,18 +3,20 @@
 #ifndef INCLUDE_SHAREDPTR_HPP_
 #define INCLUDE_SHAREDPTR_HPP_
 
-#include <cstdio>
-#include "atomic"
-#include "utility"
+#include <atomic>
+#include <iostream>
+#include <utility>
+
 template <typename T>
 class SharedPtr {
+
  private:
-  T* _ptr;
-  std::atomic_uint* _counter;
+  T* p; //указатель
+  std::atomic_uint* c; //счетчик указателей
 
  public:
   SharedPtr();
-  explicit SharedPtr(T* ptr);
+  SharedPtr(T* ptr);
   SharedPtr(const SharedPtr& r);
   SharedPtr(SharedPtr&& r);
   ~SharedPtr();
@@ -30,114 +32,116 @@ class SharedPtr {
   void reset();
   void reset(T* ptr);
   void swap(SharedPtr& r);
-  // возвращает количество объектов SharedPtr,
-  // которые ссылаются на тот же управляемый объект
-  auto use_count() const -> size_t;
+  // возвращает количество объектов SharedPtr, которые ссылаются на тот же управляемый объект
+  [[nodiscard]] auto use_count() const -> size_t;
 };
 
 template<typename T>
-SharedPtr<T>::SharedPtr()
-    : _ptr(nullptr),
-      _counter(nullptr)
-{
+SharedPtr<T>::SharedPtr() {
+  p= nullptr;
+  c= nullptr;
 }
 
 template<typename T>
-SharedPtr<T>::SharedPtr(T* ptr)
-    : _ptr(ptr),
-      _counter(new std::atomic_uint{1})
-{
+SharedPtr<T>::SharedPtr(T *ptr) {
+  p= ptr;
+  c= new std::atomic_uint{1};
 }
 
 template<typename T>
-SharedPtr<T>::SharedPtr(const SharedPtr& r)
-{
-  *this = r;
+SharedPtr<T>::SharedPtr(const SharedPtr &r) {
+  c= nullptr;
+  *this=r;
 }
 
 template<typename T>
-SharedPtr<T>::SharedPtr(SharedPtr&& r)
-{
-  *this = std::move(r);
+SharedPtr<T>::SharedPtr(SharedPtr &&r) {
+  c= nullptr;
+  *this=std::move(r);
 }
 
 template<typename T>
-SharedPtr<T>::~SharedPtr()
-{
-  if (_counter != nullptr) _counter--;
-
-  if (_counter == 0){
-    delete _counter;
-    delete _ptr;
+SharedPtr<T>::~SharedPtr() {
+  if (c == nullptr)
+    return;
+  (c)--;
+  if (c == 0) {
+    delete p;
+    delete c;
   }
 }
 
 template<typename T>
-auto SharedPtr<T>::operator=(const SharedPtr& r) -> SharedPtr&{
-  if (this != &r) {
-    this->~SharedPtr();
-    _ptr = r._ptr;
-    _counter = r._counter;
-    if (_counter != nullptr) (*_counter)++;
-  }
+auto SharedPtr<T>::operator=(const SharedPtr &r) -> SharedPtr & {
+  if(this==&r)
+    return *this;
+
+  this->~SharedPtr();
+
+  p=r.p;
+  c=r.c;
+  (*c)++;
+
   return *this;
 }
 
 template<typename T>
-auto SharedPtr<T>::operator=(SharedPtr&& r) -> SharedPtr&{
-  if (this != &r) {
-    this->~SharedPtr();
-    _ptr = r._ptr;
-    _counter = r._counter;
-    r._counter = nullptr;
-    r._ptr = nullptr;
-  }
+auto SharedPtr<T>::operator=(SharedPtr &&r) -> SharedPtr & {
+  if(this==&r)
+    return *this;
+
+  this->~SharedPtr();
+
+  p=r.p;
+  c=r.c;
+  r.c= nullptr;
+  r.p= nullptr;
+
   return *this;
 }
 
 template<typename T>
 SharedPtr<T>::operator bool() const {
-  return (_ptr != nullptr);
+  return p != nullptr;
 }
 
 template<typename T>
 auto SharedPtr<T>::operator*() const -> T & {
-  return *_ptr;
+  return *p;
 }
 
 template<typename T>
 auto SharedPtr<T>::operator->() const -> T * {
-  return _ptr;
+  return p;
 }
 
 template<typename T>
-auto SharedPtr<T>::get() -> T*{
-  return _ptr;
+auto SharedPtr<T>::get() -> T * {
+  return p;
 }
 
 template<typename T>
-void SharedPtr<T>::reset(){
-  *this = SharedPtr();
+void SharedPtr<T>::reset() {
+  *this= SharedPtr();
 }
 
 template<typename T>
-void SharedPtr<T>::reset(T* ptr){
-  *this = SharedPtr(ptr);
+void SharedPtr<T>::reset(T *ptr) {
+  *this= SharedPtr(ptr);
 }
 
 template<typename T>
-void SharedPtr<T>::swap(SharedPtr<T>& r){
-  std::swap(_ptr, r._ptr);
-  std::swap(_counter, r._counter);
+void SharedPtr<T>::swap(SharedPtr &r) {
+  std::swap(p, r.p);
+  std::swap(c, r.c);
 }
 
 template<typename T>
-auto SharedPtr<T>::use_count() const -> size_t{
-  if (_counter == nullptr){
+auto SharedPtr<T>::use_count() const -> size_t {
+  if(c!= nullptr)
+    return *c;
+  else
     return 0;
-  }
-  return *_counter;
 }
 
 #endif // INCLUDE_SHAREDPTR_HPP_
-
